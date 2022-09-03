15b3ad1898de48fc62e28d3a4cf7d5aaf8b9924d
alibagherinia <alibaghn@gmail.com>
2022-09-02 18:14:28 -0700

    added skeleton

--- PizzaMaker/ViewController.swift
+++ PizzaMaker/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mushroomView: UIImageView!
+    @IBOutlet weak var pepperView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let dragInteraction = UIDragInteraction(delegate: self)
+        mushroomView.addInteraction(dragInteraction)
+        //pepperView.addInteraction(dragInteraction)
     }
-
-
+    
 }
+
+//MARK: - UIDragInteracionDelegate
+extension ViewController: UIDragInteractionDelegate {
+    
+    func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+        guard let image = mushroomView.image else {return[]}
+        let provider = NSItemProvider(object: image)
+        let item = UIDragItem(itemProvider: provider)
+        item.localObject = image
+        print("mushroom dragged")
+        return [item]
+    }
+    
+}
+
+
 
